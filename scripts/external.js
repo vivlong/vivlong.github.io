c23ea4957d8764b5100724c0755d4c173b6f3878
vivlong <vivlonglz@gmail.com>
2014-04-17 11:49:44 +0800

    ref='external'

---
+++ scripts/external.js
@@ -0,0 +1,10 @@
+function externalLinks() {
+	if (!document.getElementsByTagName) return;
+	var anchors = document.getElementsByTagName(¡°a¡±);
+	for (var i=0; i<ANCHORS.LENGTH; i++) {
+		var anchor = anchors;
+		if (anchor.getAttribute(¡°href¡±) && anchor.getAttribute(¡°rel¡±) == ¡°external¡±)
+			anchor.target = ¡°_blank¡±;
+	}
+}
+window.onload = externalLinks;
